62023c3c3ae3fdbf59fc1eb1b570e6e273036db2
1NJ57C3 <85198277+1NJ57C3@users.noreply.github.com>
2021-12-14 01:34:51 -0800

    commit tree 57a075c921eb30e7945e193ea0e997207b311288

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,52 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from './parks/MesaVerde';
+// import { trees, wildlife } from './parks/RockyMountain';
+import * as RMFunctions from './parks/RockyMountain';
+// import {
+//   trees as parkTrees,
+//   wildlife as parkWildlife,
+// } from './parks/RockyMountain';
 
-function ColoradoStateParks() {
-  howManyParks(); // => "42 parks!"
 
-  return <h1>Colorado State Parks!</h1>;
+
+/* Ex5 - import {variable} from */
+// console.log(parkTrees);
+
+// parkWildlife();
+
+
+
+/* Ex4 - import *(all) from */
+// console.log(RMFunctions.trees);
+
+// RMFunctions.wildlife();
+
+// RMFunctions.elevation();
+
+
+/* Ex3 - named exports */
+// console.log(trees);
+
+// wildlife();
+
+
+/* Ex2 + Ex6*/
+export default function ColoradoStateParks() {
+// function ColoradoStateParks() {
+  return (
+    <div>
+      <MesaVerde />
+    </div>
+  );
 }
+
+// export default ColoradoStateParks;
+
+
+/* Ex1 */
+// function ColoradoStateParks() {
+//   howManyParks(); // => "42 parks!"
+
+//   return <h1>Colorado State Parks!</h1>;
+// }
